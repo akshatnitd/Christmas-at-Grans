979bf168fb255d7e2dc4d7676ac5a5841c02e6cb
ccarroll91 <conor-carroll@hotmail.com>
2021-03-05 11:39:47 +0000

    added empty second page, testing full screen capabilities of carousel

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -48,19 +48,73 @@ var $this = $('#myCarousel');
     } 
 }
 
-
-var elem = document.getElementById("myCarousel");
+/*
+var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
+  } else if (elem.webkitRequestFullscreen) { 
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
+  } else if (elem.msRequestFullscreen) { 
     elem.msRequestFullscreen();
   }
 }
 
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { 
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { 
+    document.msExitFullscreen();
+  }
+}
+*/
 
+function getSize(){
+          var height = img.height;
+          var width = img.width;
+          var w = window.innerWidth;
+          var h = window.innerHeight;
+
+          var adjH = height * (w/width);
+          var adjW = w;
+          
+          if(adjH>h){
+              var wRatio = h/adjH;  
+              adjH = h;
+              adjW = wRatio * w;
+          }
+      
+         document.getElementById('myImg').height = adjH;
+         document.getElementById('myImg').width = adjW;
+     }
+function requestFullScreen(element) {
+         
+         if(fullScreen==false){
+             fullScreen=true;
+
+    var requestMethod = element.requestFullScreen;
+
+    if (requestMethod) {
+        requestMethod.call(element);
+    } 
+    else{
+         fullScreen=false;
+         
+         if(document.cancelFullScreen){
+		    document.cancelFullScreen();
+		} else if(document.exitFullScreen){
+		    document.exitFullScreen();
+		} else if(document.mozCancelFullScreen){
+		    document.mozCancelFullScreen();
+		} else if(document.webkitCancelFullScreen){
+		    document.webkitCancelFullScreen();
+		} else if(document.msExitFullscreen){
+		    document.msExitFullscreen();
+		}         
+     }         
+}
 
 document.querySelector('button').addEventListener('click', toggle)
 
@@ -73,26 +127,3 @@ function toggle(event) {
     document.getElementById('about').style.display = 'none'
   }
 }
-
-/*-----------------------------------------------------------audio array - audio to be uploaded, will follow said format*/
-
-
-var audioPlayer = document.getElementById('poemAudio');
-audioPlayer.play();
-audioPlayer.pause();
-
-console.log(audioPlayer.currentTime);
-audioPlayer.currentTime = 0;
-console.log(audioPlayer.currentTime);
-
-audioPlayer.play();
-setTimeout(function () {
-  console.log(audioPlayer.currentTime);
-}, 1000);
-
-var segments = [];
-
-var audio; 
-
-var segmentEnd = 0;
-
